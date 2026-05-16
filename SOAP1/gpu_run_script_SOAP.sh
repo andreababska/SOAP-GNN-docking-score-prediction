76beb669d4e0d0f44e0374a53afdd1efe15677fa
andreababska <xbabska@stuba.sk>
2026-05-16 12:31:39 +0200

    Add files via upload
    SOAP codes

---
+++ SOAP1/gpu_run_script_SOAP.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=p1156-25-1  # Project number
+#SBATCH --job-name=gpu_test_run # Name of the job in SLURM
+#SBATCH --partition=gpu
+#SBATCH -o result.txt
+#SBATCH -e error.txt
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=16 # Number of CPU tasks
+##SBATCH --mem=60000 # Max memory allocated in MB
+##SBATCH --nodelist=n142
+
+
+module load singularity 
+
+
+time singularity exec --nv -B /projects/p1156-25-1/ /projects/p1156-25-1/DimeNet/tf_gpu_03_25.sif python3 SOAP1_in_vitro.py
+
+
+
+
+
+
+
+
+
